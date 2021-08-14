cddf5a81bd8ca544e32c536b3f39d45a15e06c48
Christine Jeon <csjeon28@gmail.com>
2021-08-14 00:48:07 -0400

    commit tree af793cfeaf51b1ed02288ed902a072303f8d9c0c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,32 @@ class BirdsController < ApplicationController
     end
   end
 
+  # PATCH /birds/:id
+  def update
+    # find bird that matches id param
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird 
+    else
+      render json: {error: "Bird Not Found"}, status: :not_found
+    end
+  end
+
+  def increment_likes
+    # find bird that we're trying to update
+    bird = Bird.find_by(id: params[:id])
+    # update the bird using the number of CURRENT likes + 1 to determine next number of likes
+    bird.update(likes: bird.likes + 1)
+    render json: bird 
+  end
+
+  
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
